9da2054e76b69b229ea9d774a51cdcc081dc760c
Matt Hinchliffe <matt@maketea.co.uk>
2012-12-06 12:53:39 +0000

    Use parent .outerHeight(), update to idiomatic code style

--- js/modal.js
+++ js/modal.js
@@ -1,271 +1,231 @@
-/**
+/*!
  * @name        jQuery Modal
  * @author      Matt Hinchliffe <https://github.com/i-like-robots/jQuery-Modal>
- * @modified    13/09/2012
- * @version     1.0.0b
- * @description A simple modal overlay
- *
- * @example jQuery plugin setup
- * var modal_instance = $('body').modal().data('modal');
- *
- * $.ajax({
- *     url: [url],
- *     success: function(data)
- *     {
- *         modal_instance.open(data, callback);
- *     }
- * });
- *
- * @example Generated markup
- * <div class="modal-wrapper">
- *     <span class="modal-close" data-toggle="modal">Close</span>
- *     <div class="modal-content" />
- * </div>
- * <div class="modal-overlay" data-toggle="modal" />
+ * @modified    06/12/2012
+ * @version     1.0.0
  */
-; (function($, undefined)
-{
-	"use strict";
+(function( $, undefined ) {
 
-	function Modal(target, options)
-	{
-		this.opts = $.extend({}, { // Create a new options object for each instance
-			onopen: undefined,
-			onhide: undefined,
-			onupdate: undefined,
-			fixed: false,
-			overlay: true,
-			blur: true,
-			escape: true,
-			width: 640,
-			maxWidth: '95%',
-			height: 480,
-			maxHeight: '95%'
-		}, options);
+    'use strict';
 
-		this.target = target;
+    function Modal( target, options ) {
 
-		return this;
-	}
+        this.opts = $.extend({}, {
+            onopen: undefined,
+            onhide: undefined,
+            onupdate: undefined,
+            fixed: false,
+            overlay: true,
+            blur: true,
+            escape: true,
+            width: 640,
+            maxWidth: '95%',
+            height: 480,
+            maxHeight: '95%'
+        }, options);
 
-	Modal.prototype = {
+        this.target = target;
 
-		isOpen: false,
+        return this;
+    }
 
-		isInitialized: false,
+    Modal.prototype = {
 
-		/**
-		 * Instantiate
-		 * @description Create the structure on first run
-		 */
-		_init: function()
-		{
-			this.doc = $(document);
+        isOpen: false,
 
-			this.wrapper = $('<div class="modal-wrapper" />').css({
-				position: this.opts.fixed ? 'fixed' : 'absolute',
-				width: this.opts.width,
-				maxWidth: this.opts.maxWidth,
-				height: this.opts.height,
-				maxHeight: this.opts.maxHeight,
-				display: 'none'
-			});
-			this.close = $('<span class="modal-close" data-toggle="modal">Close</span>').appendTo( this.wrapper );
-			this.content = $('<div class="modal-content" />').appendTo( this.wrapper );
+        isInitialized: false,
 
-			this.wrapper.appendTo( this.target );
+        /**
+         * Instantiate
+         * @description Create the structure on first run
+         */
+        _init: function() {
 
-			// Define overlay to prevent errors
-			this.overlay = false;
+            this.doc = $(document);
 
-			if (this.opts.overlay)
-			{
-				this.overlay = $('<div class="modal-overlay"' + (this.opts.blur ? 'data-toggle="modal"' : '') + ' />')
-					.css({
-						position: 'absolute',
-						top: 0,
-						left: 0,
-						width: '100%',
-						display: 'none'
-					})
-					.appendTo( this.target );
-			}
+            this.wrapper = $('<div class="modal-wrapper">').css({
+                position: this.opts.fixed ? 'fixed' : 'absolute',
+                width: this.opts.width,
+                maxWidth: this.opts.maxWidth,
+                height: this.opts.height,
+                maxHeight: this.opts.maxHeight,
+                display: 'none'
+            });
 
-			// Context appropriate metrics and events
-			this.isBody = (this.target === document.body);
-			this.context = this.isBody ? $(window) : $( this.target ).css('position', 'relative');
+            this.close = $('<span class="modal-close" data-toggle="modal">Close</span>').appendTo(this.wrapper);
+            this.content = $('<div class="modal-content">').appendTo(this.wrapper);
 
-			this.isInitialized = true;
-		},
+            this.wrapper.appendTo(this.target);
 
-		/**
-		 * Align
-		 * @description Centre modal window and size overlay to fit
-		 */
-		align: function()
-		{
-			var height = this.wrapper.outerHeight(),
-			    width = this.wrapper.outerWidth(),
-			    maxHeight = this.context.height(),
-			    maxWidth = this.context.width(),
-			    top = this.opts.fixed ? 0 : this.context.scrollTop();
+            // Define overlay to prevent errors
+            this.overlay = false;
 
-			this.wrapper.css(
-			{
-				top: height < maxHeight ? ( (maxHeight - height ) / 2 ) + top : top,
-				left: width < maxWidth ? (maxWidth - width) / 2 : 0
-			});
+            if ( this.opts.overlay ) {
+                this.overlay = $('<div class="modal-overlay"' + (this.opts.blur ? 'data-toggle="modal"' : '') + '>')
+                    .css({
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        display: 'none'
+                    })
+                    .appendTo(this.target);
+            }
 
-			if (this.opts.overlay)
-			{
-				this.overlay.css('height', this.isBody ? this.doc.height() : maxHeight );
-			}
-		},
+            // Context appropriate metrics and events
+            this.isBody = (this.target === document.body);
+            this.context = this.isBody ? $(window) : $(this.target).css('position', 'relative');
 
-		/**
-		 * Open
-		 * @description Open the modal window
-		 * @param {object} content
-		 * @param {function} callback
-		 */
-		open: function(content, callback)
-		{
-			if ( ! this.isInitialized)
-			{
-				this._init();
-			}
+            this.isInitialized = true;
+        },
 
-			var self = this;
+        /**
+         * Align
+         * @description Centre modal window and size overlay to fit
+         */
+        align: function() {
 
-			if (this.isBody)
-			{
-				this.context.on('resize.modal', function()
-				{
-					self.align();
-				});
+            var height = this.wrapper.outerHeight(),
+                width = this.wrapper.outerWidth(),
+                maxHeight = this.context.outerHeight(),
+                maxWidth = this.context.outerWidth(),
+                top = this.opts.fixed ? 0 : this.context.scrollTop();
 
-				this.doc.on('keyup.modal', function(e)
-				{
-					if ( e.keyCode === 27 )
-					{
-						self.hide();
-					}
-				});
-			}
+            this.wrapper.css({
+                top: height < maxHeight ? ( (maxHeight - height ) / 2 ) + top : top,
+                left: width < maxWidth ? (maxWidth - width) / 2 : 0
+            });
 
-			this.doc.on('click.modal', '[data-toggle="modal"]', function(e)
-			{
-				e.preventDefault();
-				self.hide();
-			});
+            if ( this.opts.overlay ) {
+                this.overlay.css('height', this.isBody ? this.doc.height() : maxHeight );
+            }
+        },
 
-			// Fade in
-			this.wrapper
-				.add( this.overlay )
-				.stop()
-				.fadeIn();
+        /**
+         * Open
+         * @description Open the modal window
+         * @param {object} content
+         * @param {function} callback
+         */
+        open: function( content, callback ) {
 
-			this.isOpen = true;
+            if ( ! this.isInitialized) {
+                this._init();
+            }
 
-			// Add content to window
-			if (content)
-			{
-				this.update(content);
-			}
+            var self = this;
 
+            if ( this.isBody ) {
+                this.context.on('resize.modal', function() {
+                    self.align();
+                });
 
-			// Callbacks
-			if (this.opts.onopen)
-			{
-				this.opts.onopen.call(this);
-			}
-			if (callback)
-			{
-				callback.call(this);
-			}
-		},
+                this.doc.on('keyup.modal', function( e ) {
+                    if ( e.keyCode === 27 ) {
+                        self.hide();
+                    }
+                });
+            }
 
-		/**
-		 * Update
-		 * @description Update the modal window contents
-		 * @param {object|string} content
-		 * @param {function} callback
-		 */
-		update: function(content, callback)
-		{
-			this.content.html(content);
+            this.doc.on('click.modal', '[data-toggle="modal"]', function( e ) {
+                e.preventDefault();
+                self.hide();
+            });
 
-			if (this.isOpen)
-			{
-				this.align();
-			}
+            // Fade in
+            this.wrapper
+                .add(this.overlay)
+                .stop()
+                .fadeIn();
 
-			// Callbacks
-			if (this.opts.onupdate)
-			{
-				this.opts.onupdate.call(this);
-			}
-			if (callback)
-			{
-				callback.call(this);
-			}
-		},
+            this.isOpen = true;
 
-		/**
-		 * Resize
-		 * @description Resizes the modal window content area
-		 * @param {numeric|string} width
-		 * @param {numeric|string} height
-		 */
-		resize: function(width, height)
-		{
-			this.wrapper.css({
-				width: width,
-				height: height
-			});
+            // Add content to window
+            if ( content ) {
+                this.update(content);
+            }
 
-			this.align();
-		},
+            // Callbacks
+            if ( this.opts.onopen ) {
+                this.opts.onopen.call(this);
+            }
+            if ( callback ) {
+                callback.call(this);
+            }
+        },
 
-		/**
-		 * Hide
-		 * @description Hide the modal window
-		 * @param {function} callback
-		 */
-		hide: function(callback)
-		{
-			// Unbind events
-			this.doc.off('.modal');
+        /**
+         * Update
+         * @description Update the modal window contents
+         * @param {object|string} content
+         * @param {function} callback
+         */
+        update: function( content, callback ) {
 
-			this.wrapper
-				.add( this.overlay )
-				.stop()
-				.fadeOut();
+            this.content.html(content);
 
-			this.isOpen = false;
+            if ( this.isOpen ) {
+                this.align();
+            }
 
-			// Callbacks
-			if (this.opts.onhide)
-			{
-				this.opts.onhide.call(this);
-			}
-			if (callback)
-			{
-				callback.call(this);
-			}
-		}
-	};
+            // Callbacks
+            if ( this.opts.onupdate ) {
+                this.opts.onupdate.call(this);
+            }
+            if ( callback )
+            {
+                callback.call(this);
+            }
+        },
 
-	// jQuery plugin wrapper
-	$.fn.modal = function(options)
-	{
-		return this.each(function()
-		{
-			if ( ! $.data(this, 'modal') )
-			{
-				$.data(this, 'modal', new Modal(this, options) );
-			}
-		});
-	};
+        /**
+         * Resize
+         * @description Resizes the modal window content area
+         * @param {numeric|string} width
+         * @param {numeric|string} height
+         */
+        resize: function( width, height ) {
+            this.wrapper.css({
+                width: width,
+                height: height
+            });
+
+            this.align();
+        },
+
+        /**
+         * Hide
+         * @description Hide the modal window
+         * @param {function} callback
+         */
+        hide: function( callback ) {
+            // Unbind events
+            this.doc.off('.modal');
+
+            this.wrapper
+                .add(this.overlay)
+                .stop()
+                .fadeOut();
+
+            this.isOpen = false;
+
+            // Callbacks
+            if ( this.opts.onhide ) {
+                this.opts.onhide.call(this);
+            }
+            if ( callback ) {
+                callback.call(this);
+            }
+        }
+    };
+
+    // jQuery plugin wrapper
+    $.fn.modal = function( options ) {
+        return this.each(function() {
+            if ( ! $.data(this, 'modal') ) {
+                $.data(this, 'modal', new Modal(this, options) );
+            }
+        });
+    };
 
 })(jQuery);
